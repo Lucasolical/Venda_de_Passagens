c1d254f69798cb07bfe943b2e7e8cef5d08e6e5f
lucas oliveira <locbeso@gmail.com>
2025-10-12 13:44:07 -0300

    conexão entre login com pagina de usuario

--- Venda_de_passagens/routes.py
+++ Venda_de_passagens/routes.py
@@ -1,8 +1,14 @@
 from main import app
-from flask import render_template
+from flask import render_template,request, redirect, url_for
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    usuario = request.form["usuario"]
+    # Aqui você poderia validar com seu dicionário, se quiser
+    return redirect(url_for("usuarios", nome_usuario=usuario))
 
 @app.route("/contatos")
 def contatos():
@@ -10,4 +16,5 @@ def contatos():
 
 @app.route("/usuario/<nome_usuario>")
 def usuarios(nome_usuario):
+    
     return render_template("usuariopage.html", nome_usuario = nome_usuario)
