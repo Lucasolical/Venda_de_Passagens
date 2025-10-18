d551b0fe5ea1d7f65444cd97555c1ab093369404
lucas oliveira <locbeso@gmail.com>
2025-10-18 10:05:48 -0300

    implementação do dicionario com login da pagina

--- Venda_de_passagens/routes.py
+++ Venda_de_passagens/routes.py
@@ -1,5 +1,7 @@
 from main import app
 from flask import render_template,request, redirect, url_for
+from dicionarioVoo import*
+
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
@@ -7,8 +9,11 @@ def homepage():
 @app.route("/login", methods=["POST"])
 def login():
     usuario = request.form["usuario"]
-    # Aqui você poderia validar com seu dicionário, se quiser
-    return redirect(url_for("usuarios", nome_usuario=usuario))
+    senha = request.form["senha"]
+    for user in logins:
+        if usuario == user["nome"] and senha == user["senha"]:
+            return redirect(url_for("usuarios", nome_usuario=user["nome"]))
+    return render_template("homepage.html", erro="Usuário ou senha incorretos!")
 
 @app.route("/contatos")
 def contatos():
@@ -16,5 +21,4 @@ def contatos():
 
 @app.route("/usuario/<nome_usuario>")
 def usuarios(nome_usuario):
-    
     return render_template("usuariopage.html", nome_usuario = nome_usuario)
