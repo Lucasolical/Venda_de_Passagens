be86a5ece996f9cd4f785c386e9bd147d9fe4845
Luis Jackson <luisjacksonjr@gmail.com>
2025-10-27 19:29:42 -0300

    Modificado homepage p/index

--- Venda_de_passagens/routes.py
+++ Venda_de_passagens/routes.py
@@ -1,10 +1,16 @@
 from main import app
-from flask import render_template,request, redirect, url_for
-from dicionarioVoo import*
+from flask import render_template, request, redirect, url_for
+from dicionarioVoo import voos, logins
 
 @app.route("/")
 def homepage():
-    return render_template("homepage.html")
+    return render_template("index.html", 
+                           lista_de_voos=voos.items(), 
+                           search_terms={})
+
+@app.route("/admin_login")
+def admin_login_page():
+    return render_template("login.html")
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -13,25 +19,43 @@ def login():
     for user in logins:
         if usuario == user["nome"] and senha == user["senha"]:
             return redirect(url_for("usuarios", nome_usuario=user["nome"]))
-    return render_template("homepage.html", erro="Usuário ou senha incorretos!")
+    return render_template("login_admin.html", erro="Usuário ou senha incorretos!")
 
-@app.route("/contatos")
-def contatos():
-    return render_template("contatos.html")
+@app.route("/buscar_voos")
+def buscar_voos():
+
+    origem_filtro = request.args.get('origem', '').lower()
+    destino_filtro = request.args.get('destino', '').lower()
+    
+    # (adicionar data_ida e data_volta aqui depois)
+
+    voos_filtrados = {}
+    
+    for codigo, voo in voos.items():
+        origem_match = origem_filtro in voo['origem'].lower()
+        destino_match = destino_filtro in voo['destino'].lower()
+        
+        if origem_match and destino_match:
+            voos_filtrados[codigo] = voo
+            
+    return render_template("index.html", 
+                           lista_de_voos=voos_filtrados.items(),
+                           search_terms={'origem': request.args.get('origem', ''), 
+                                         'destino': request.args.get('destino', '')})
+
 
 @app.route("/usuario/<nome_usuario>")
 def usuarios(nome_usuario):
-    return render_template("usuariopage.html", nome_usuario = nome_usuario)
+    return render_template("adminpage.html", nome_usuario=nome_usuario)
 
 @app.route("/voos")
 def listar_voos():
     return render_template("listar_voos.html", lista_de_voos=voos)
 
-
 @app.route("/usuario/<nome_usuario>/voos")
-def listar_voos_para_usuario(nome_usuario):
-    return render_template("listar_voos.html", 
-                           lista_de_voos=voos,
+def listar_voos_para_admin(nome_usuario):
+    return render_template("listar_voos_admin.html", 
+                           lista_de_voos=voos, 
                            nome_usuario=nome_usuario)
 
 @app.route("/cadastro")
